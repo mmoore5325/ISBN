13f8cc4ff997ec8a192dd69e3ac2495fbb109734
Michael Moore <Michael Moore>
2016-09-30 18:25:07 -0400

    Another method of seperating string of numbers into array

--- isbn_10_function.rb
+++ isbn_10_function.rb
@@ -1,6 +1,13 @@
 print "Give me a 10 digit ISBN"
 isbn10 = gets.chomp
-b = isbn10.to_s.split(//).map{|isbn10| isbn10.to_i}
-puts "#{b}"
+isbn10 = isbn10.to_s.chars.map(&:to_i)
+puts "#{isbn10}"
+a = 12345.to_s.chars.map(&:to_i)
+puts "#{a}"
+
+# b = isbn10.to_s.split(//).map{|isbn10| isbn10.to_i}
+# puts "#{b}"
+# c = b.first
+# puts "#{c}"
 
 
