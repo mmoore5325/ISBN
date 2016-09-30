0b2bd06e4e48b06962f9027b51885276e796d197
Michael Moore <Michael Moore>
2016-09-30 18:52:25 -0400

    isbn_10_function.rb will successfully output the check digit, no X value added yet.

--- isbn_10_function.rb
+++ isbn_10_function.rb
@@ -2,8 +2,12 @@ print "Give me a 10 digit ISBN"
 isbn10 = gets.chomp
 isbn10 = isbn10.to_s.chars.map(&:to_i)
 puts "#{isbn10}"
-a = 12345.to_s.chars.map(&:to_i)
-puts "#{a}"
+ans = 0
+isbn10.each_with_index {| num, idx | ans += (idx + 1) * num }
+puts "#{ans}"
+c = ans % 11
+puts "#{c}"
+
 
 # b = isbn10.to_s.split(//).map{|isbn10| isbn10.to_i}
 # puts "#{b}"
