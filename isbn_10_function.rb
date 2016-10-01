35210fa524e12ee7827b386e95f96e0084d030ec
Michael Moore <Michael Moore>
2016-09-30 20:11:50 -0400

    Last output prints out the ISBN 10 number with a check digit

--- isbn_10_function.rb
+++ isbn_10_function.rb
@@ -1,9 +1,9 @@
 print "Give me a 10 digit ISBN"
 isbn10 = gets.chomp
-isbn10 = isbn10.to_s.chars.map(&:to_i)
+d = isbn10.to_s.chars.map(&:to_i)
 puts "#{isbn10}"
 ans = 0
-isbn10.each_with_index {| num, idx | ans += (idx + 1) * num }
+d.each_with_index {| num, idx | ans += (idx + 1) * num }
 puts "#{ans}"
 c = ans % 11
 puts "#{c}"
@@ -11,6 +11,7 @@ if c == 10
 	c = "x"
 end
 puts "#{c}"
+puts "#{isbn10}#{c}"
 # b = isbn10.to_s.split(//).map{|isbn10| isbn10.to_i}
 # puts "#{b}"
 # c = b.first
