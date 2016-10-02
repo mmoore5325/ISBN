9957b536789fa947efa079d4aa45b4ef0cfdbdf5
Michael Moore <Michael Moore>
2016-10-02 17:34:08 -0400

    Got both giving the checkdigit and full ISBN number.  Code is sloppy, I dug into it and made it all sloppy trying to figure out what was wrong, only to realize I had done something to the ERB file and hadnt saved it.  will clean up later.

--- isbnmain.rb
+++ isbnmain.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require_relative 'isbn_13_function.rb'
-
+require_relative 'isbn_10_function.rb'
 get '/' do
 	erb :isbnchoose
 end
@@ -8,24 +8,23 @@ end
 post '/isbnchoose' do
 	isbn = params[:isbntype]
 
-	if isbn == "ISBN-10"
-		erb :isbn_10
-	elsif isbn == "ISBN-13"
-		erb :isbn_13
+	if isbn == "ISBN-13"
+		redirect '/isbn_13'
+	else isbn == "ISBN-10"
+		redirect '/isbn_10'
 	end
 end
 
 get '/isbn_13' do
 
 	erb :isbn_13
-	isbn14 = isbn13
-	isbn14.to_s.chars.map(&:to_i)		#This code is the exact same as above, just slightly adjusted for 
 	
-	if isbn14.length > 12			#the isbn-13 file.
-		"Too many numbers.  Soon I will make a page that takes you back but not yet."
-	else
-		erb :isbn13cal
-	end
+	# isbn14 = isbn13
+	# isbn14.to_s.chars.map(&:to_i)		#This code is the exact same as above, just slightly adjusted for 
+	
+	# if isbn14.length > 12			#the isbn-13 file.
+	# 	"Too many numbers.  Soon I will make a page that takes you back but not yet."
+	# end
 
 end
 
@@ -40,6 +39,54 @@ post '/isbn_13' do
 
 	# redirect '/isbn13cal?isbn_13=' + isbn13x
 end
+
+get '/isbn_10' do
+
+	erb :isbn_10
+	
+	# isbn11 = isbn10x
+	# isbn11.to_s.chars.map(&:to_i)		#This code is the exact same as above, just slightly adjusted for 
+	
+	# if isbn11.length > 9			#the isbn-13 file.
+	# 	"Too many numbers.  Soon I will make a page that takes you back but not yet."
+	# end
+
+end
+# get '/isbn_10' do
+	
+# 	erb :isbn_10
+	
+# 	isbn11 = isbn10x
+# 	isbn11.to_s.chars.map(&:to_i)
+
+# 	if isbn11.length > 9
+# 		"Too many numbers."
+# 	end
+
+# end
+
+post '/isbn_10' do
+
+	isbn10x = params[:isbn_10]
+	isbn10digit = isbn10(isbn10x)
+	erb :isbn13cal, :locals => {:isbn_10 => isbn10x}
+
+	isbn10digit = isbn10(isbn10x)
+	"Your isbn is #{isbn10digit[:isbn]}#{isbn10digit[:checkdig]}, with your check digit being #{isbn10digit[:checkdig]}."
+
+	# redirect '/isbn13cal?isbn_13=' + isbn13x
+end
+
+# post '/isbn_10' do
+
+# 	isbn10x = params[:isbn_10]
+# 	isbn10digit = isbn10(isbn10x)
+# 	erb :isbn_10
+
+# 	isbn10digit = isbn10(isbn10x)
+# 	"Your isbn is #{isbn10digit[:isbn_13]}#{isbn10digit[:checkdig]}, with your check digit being #{isbn10digit[:checkdig]}."
+# end
+
 
 # get '/isbn13cal' do
 
