65a881050dba51d8b1887c65a7b53d60befcdaf8
Michael Moore <Michael Moore>
2016-10-02 14:07:18 -0400

    Beginning to focus on webpage.  Currently have mainpage choose isbn-13 or 10, choosing 13 pops up a submit box that refuses too many or too little characters.

--- isbnmain.rb
+++ isbnmain.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
+require_relative 'isbn_13_function.rb'
 
 get '/' do
 	erb :isbnchoose
 end
 
-post '/isbntype' do
+post '/isbnchoose' do
 	isbn = params[:isbntype]
 
 	if isbn == "ISBN-10"
@@ -13,4 +14,30 @@ post '/isbntype' do
 		erb :isbn_13
 	end
 end
+
+get '/isbn_13' do
+
+	erb :isbn_13
+
+end
+
+post '/isbn_13' do
+
+	isbn13 = params[:isbn_13]
+	isbn14 = isbn13
+	isbn14.to_s.chars.map(&:to_i)		#This code is the exact same as above, just slightly adjusted for 
+	if isbn14.length > 12			#the isbn-13 file.
+		"Too many numbers. Taking you back..."
+		<meta http-equiv="refresh" content="3;/isbn_13" />
+	end
+
+	else
+		erb :isbn13cal
+	end
+
+
+end
+
+
+
 
