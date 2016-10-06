88fbe796c97fdb4fc9bce1358cae038c09c3ad10
mmoore5325 <mmoore5325@gmail.com>
2016-10-06 12:26:13 -0700

    first assert_equal for 13 works fine testing lengt

--- isbnv2.rb
+++ isbnv2.rb
@@ -1,36 +1,42 @@
 require "minitest/autorun"
 require_relative "isbnv2test.rb"
+require_relative "isbnv2_13.rb"
 
 class TestIsbn <Minitest::Test
 	
 	def test_valid_ten_digit_isbn_returns_true
 		isbn = "0471958697"
-		assert_equal(true, valid_isbn?(isbn))
+		assert_equal(true, valid_isbn_10?(isbn))
 	end
 
 	def test_empty_string_returns_false
 		isbn = ""
-		assert_equal(false, valid_isbn?(isbn))
+		assert_equal(false, valid_isbn_10?(isbn))
 	end
 
 	def test_valid_isbn_with_spaces_returns_true
 		isbn = "0 47 19 58 697"
-		assert_equal(true, valid_isbn?(isbn))
+		assert_equal(true, valid_isbn_10?(isbn))
 	end
 
 	def test_valid_isbn_with_dashes_returns_true
 		isbn = "0-47-19-58-697"
-		assert_equal(true, valid_isbn?(isbn))
+		assert_equal(true, valid_isbn_10?(isbn))
 	end
 
 	def test_invalid_isbn_10_returns_false
 		isbn = "0471598699"
-		assert_equal(false, validate_isbn?(isbn))
+		assert_equal(false, validate_isbn_10?(isbn))
 	end
 
 	def test_valid_isbn_10_returns_true
 		isbn = "123456789x"
-		assert_equal(true, validate_isbn?(isbn))
+		assert_equal(true, validate_isbn_10?(isbn))
+	end
+
+	def test_valid_13_digit_isbn_returns_true
+		isbn = "1234567891234"
+		assert_equal(true, valid_isbn_13?(isbn))
 	end
 end
 	
--- isbnv2_13.rb
+++ isbnv2_13.rb
@@ -1,46 +1,44 @@
+def valid_isbn_13?(isbn)
+	remove_spaces(isbn)
+	remove_dashes(isbn)
+
+	isbn.length == 13 ? true : false
+end
+
+def remove_spaces(isbn)
+	isbn.gsub!(" ", "")
+end
+
+def remove_dashes(isbn)
+	isbn.gsub!("-", "")
+end
+
 def validate_isbn_13?(isbn)
 
-	def remove_spaces
-		isbn.gsub!(" ", "")
-	end
+	valid_isbn_13?(isbn)
 
-	def remove_dashes(isbn)
-		isbn.gsub!("-", "")
-	end
+	isbn = isbn.to_s.chars.map(&:to_i)
 
-	def valid_isbn_13?(isbn)
+	checkdigit = isbn.last
 
-		remove_spaces(isbn)
-		remove_dashes(isbn)
+	isbn.pop
 
-		isbn.length == 10 ? true : false
-	end
+	sum = 0
 
-	def validate_isbn_13?(isbn)
-
-		valid_isbn_13?(isbn)
-
-		isbn = isbn.to_s.chars.map(&:to_i)
-
-		checkdigit = isbn.last
-
-		isbn.pop
-
-		sum = 0
-
-		isbn.each_with_index do |value, index|
-			if index % 2 == 0
-				value = value
-			else
-				value = 3 * value
-			end
-			sum += value
+	isbn.each_with_index do |value, index|
+		if index % 2 == 0
+			value = value
+		else
+			value = 3 * value
 		end
-
-		checkdigit2 = 10 - (sum % 10)
-
-		checkdigit == checkdigit2 ? true : false
+		sum += value
 	end
+
+	checkdigit2 = 10 - (sum % 10)
+
+	checkdigit == checkdigit2 ? true : false
+end
+
 
 
 
