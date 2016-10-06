73dfc3ef3011ceb81a67a47de45e6867021f6674
mmoore5325 <mmoore5325@gmail.com>
2016-10-06 12:14:03 -0700

    Created isbnv2_13.rb.  Tested in IRB, seems working.  Going to create assert_equal.

---
+++ isbnv2_13.rb
@@ -0,0 +1,46 @@
+def validate_isbn_13?(isbn)
+
+	def remove_spaces
+		isbn.gsub!(" ", "")
+	end
+
+	def remove_dashes(isbn)
+		isbn.gsub!("-", "")
+	end
+
+	def valid_isbn_13?(isbn)
+
+		remove_spaces(isbn)
+		remove_dashes(isbn)
+
+		isbn.length == 10 ? true : false
+	end
+
+	def validate_isbn_13?(isbn)
+
+		valid_isbn_13?(isbn)
+
+		isbn = isbn.to_s.chars.map(&:to_i)
+
+		checkdigit = isbn.last
+
+		isbn.pop
+
+		sum = 0
+
+		isbn.each_with_index do |value, index|
+			if index % 2 == 0
+				value = value
+			else
+				value = 3 * value
+			end
+			sum += value
+		end
+
+		checkdigit2 = 10 - (sum % 10)
+
+		checkdigit == checkdigit2 ? true : false
+	end
+
+
+
